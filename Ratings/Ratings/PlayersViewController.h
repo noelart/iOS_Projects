3c23ffd4ee6d47b175f47b613e292f2fdc15af74
Noel Zahra <noelz@uw.edu>
2014-01-14 00:16:14 +0100

    Add Ratings app

---
+++ Ratings/Ratings/PlayersViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlayersViewController.h
+//  Ratings
+//
+//  Created by Noel Zahra on 13/01/2014.
+//  Copyright (c) 2014 Noel Zahra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersViewController : UITableViewController
+
+@property(nonatomic, strong) NSMutableArray *players;
+
+@end
---
+++ Ratings/Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by Noel Zahra on 13/01/2014.
+//  Copyright (c) 2014 Noel Zahra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
